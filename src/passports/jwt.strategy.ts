5779d27cb2a65062e55c70d0d210618e2fd6c37a
Cuong Vu <156158430+CuongVu1920@users.noreply.github.com>
2026-07-12 03:11:27 +0700

    fix(auth): validate JWT by check blacklist

--- src/passports/jwt.strategy.ts
+++ src/passports/jwt.strategy.ts
@@ -1,7 +1,7 @@
+import { RedisService } from './../modules/redis/redis.service';
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-
 import { HttpStatus } from '@nestjs/common';
 import { UserService } from '../modules/user/user.service';
 import { jwtConfig } from '../configs/jwt.config';
@@ -9,10 +9,15 @@ import { JwtPayload } from '../modules/auth/type/jwt-payload.type';
 import { UserStatus } from '../generated/prisma/browser';
 import { AppException } from '../exceptions/app.exception';
 import { TOURIFY_ERROR_CODES } from '../constants/error-code.constant';
+import { CACHE } from '../constants/cache.constant';
+import { UserResponse } from '../common/responses/user.response';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly userService: UserService) {
+  constructor(
+    private readonly userService: UserService,
+    private readonly redisService: RedisService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -20,7 +25,21 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: JwtPayload) {
+  async validate(payload: JwtPayload): Promise<UserResponse> {
+    if (payload.jti) {
+      const redisClient = this.redisService.getClient();
+      const blacklisted = await redisClient.get(
+        CACHE.AUTH._KEY.BLACKLIST(payload.jti),
+      );
+
+      if (blacklisted) {
+        throw new AppException(
+          TOURIFY_ERROR_CODES.AUTH.INVALID_CREDENTIALS,
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
+    }
+
     const user = await this.userService.findById(payload.sub);
 
     if (!user || user.status !== UserStatus.ACTIVE) {
@@ -31,6 +50,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...result } = user;
-    return result;
+    return new UserResponse(result);
   }
 }
